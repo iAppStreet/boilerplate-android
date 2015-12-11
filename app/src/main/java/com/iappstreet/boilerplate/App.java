7016a6fb1c6013dbd5b5d94b0e29ca36c5c53bca
Pranav Chaturvedi <pranav.g10@gmail.com>
2015-12-11 16:11:18 +0530

    Partial commit of 'app': App and manifest only

---
+++ app/src/main/java/com/iappstreet/boilerplate/App.java
@@ -0,0 +1,21 @@
+package com.iappstreet.boilerplate;
+
+import com.iappstreet.commons.Application;
+
+/**
+ * Copyright 2015 App Street Software Pvt. Ltd.
+ * <p/>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+public class App extends Application {
+}
